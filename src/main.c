134e6b9a75083d65883b706b16724bf38df2fbef
mohtasironom <mohtasir647@gmail.com>
2026-08-24 01:22:09 +0600

    main.c complted

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "common.h"
+#include "bmp_stego.h"
+#include "whitespace_stego.h"
 
 #define PATH_MAX_LEN 512
 #define MSG_MAX_LEN  4096
