754b1fd899c57eff05f67c69cc9f461221feb5f0
mohtasironom <mohtasir647@gmail.com>
2026-08-24 13:13:17 +0600

    add file_size function

--- src/common.c
+++ src/common.c
@@ -1,2 +1,10 @@
 #include <stdlib.h>
 #include "common.h"
+
+long file_size(FILE *fp) {
+    long cur = ftell(fp);
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, cur, SEEK_SET);
+    return size;
+}
